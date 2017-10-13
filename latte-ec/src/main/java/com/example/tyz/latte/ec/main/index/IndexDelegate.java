3979f5ff811678eb925baf3882347748a7d1f110
tyz <644182537@qq.com>
2017-10-13 18:02:56 +0800

    通用底部导航

---
+++ latte-ec/src/main/java/com/example/tyz/latte/ec/main/index/IndexDelegate.java
@@ -0,0 +1,23 @@
+package com.example.tyz.latte.ec.main.index;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.example.tyz.latte.delegate.bottom.BottomItemDelegate;
+
+/**
+ * Created by Administrator on 2017/10/13.
+ */
+
+public class IndexDelegate extends BottomItemDelegate {
+    @Override
+    public Object setLayout() {
+        return null;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootview) {
+
+    }
+}
